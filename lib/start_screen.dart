e2baf7c038240c433ce487e635d3e13939aa1810
jeowsome <jeowsome15@gmail.com>
2023-04-14 01:56:48 +0800

    split class

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,49 +5,35 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 26, 2, 80),
-              Color.fromARGB(255, 45, 7, 98),
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
           ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                'assets/images/quiz-logo.png',
-                width: 300,
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              const Text(
-                'Learn Flutter the fun way!',
-                style: TextStyle(
-                  fontSize: 28,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              OutlinedButton(
-                onPressed: () {},
-                child: const Text(
-                  'Start Quiz',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ],
+          const SizedBox(
+            height: 80,
           ),
-        ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              fontSize: 24,
+              color: Color.fromARGB(255, 237, 223, 252),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            child: const Text('Start Quiz'),
+          ),
+        ],
       ),
     );
   }
