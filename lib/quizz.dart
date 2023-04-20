c9c8ee3ae6d54078f9b2f60af0e777ff280a0119
jeowsome <jeowsome15@gmail.com>
2023-04-20 14:31:00 +0800

    results screen

--- lib/quizz.dart
+++ lib/quizz.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quizz_app/data/questions.dart';
 import 'package:quizz_app/start_screen.dart';
 import 'package:quizz_app/question_screen.dart';
+import 'package:quizz_app/results_screen.dart';
 
 class Quizz extends StatefulWidget {
   const Quizz({super.key});
@@ -29,6 +30,10 @@ class _QuizzState extends State<Quizz> {
         activeScreen = StartScreen(switchScreen);
         selectedAnswers = [];
       });
+    }
+
+    if (activeScreen.runtimeType == StartScreen) {
+      activeScreen = const ResultsScreen();
     }
   }
 
